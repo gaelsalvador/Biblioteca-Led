3147b67add44d18ec4d457cec6dd4114c3a67b4f
GAEL DE ROUSSET RASPANTINI SALVADOR <24018834870@info.local>
2026-04-14 09:30:48 -0300

    Comentarios

--- src/LED.h
+++ src/LED.h
@@ -2,6 +2,13 @@
 #define LED_h
 #include <Arduino.h>
 
+/**
+ * @class led
+ * @brief Classe para controle não bloqueante de um led
+ * Permite ligar, desligar, alternar, ligar por tempo determinado, piscar continuamente, 
+ * piscar por frequencia e piscar por quantidade definida e por frequencia
+ * @note O metodo Uptade() deve ser chamado continuamente dentro do loop().
+ */
 class led
 {
     private:
@@ -10,31 +17,74 @@ class led
         bool estado;
         bool desligarportempo = false;
         uint32_t DesligarNoMomento;
+        /**
+         * @brief Processa a logica de piscar do led
+         */
         void funcaoPiscar();
         uint32_t temporizador;
         uint32_t tempoEspera;
         bool estadoPiscar;
         uint16_t repeticoes;
         float frequencia;
+        /**
+         * @brief Processa o desligamento do temporizador
+         */
         void funcaoDesligamento();
+     
     public:
-        led(uint8_t pin);//metodo construtor devera ter o mesmo nome da classe
-        
-        void ligar(); //ok
+    /**
+         * @brief Constroi um objeto led
+         * @param pin Numero do pin digital a onde o led está
+         */
+        led(uint8_t pin);
+        /**
+         * @brief liga o led continuamente
+         */
+        void ligar();
+        /**
+         * @brief liga o led por um tempo determinado
+         * @param tempo_ligado_ms determina o tempo que o led vai ficar ligado em milisegundo
+         */
         void ligar(uint32_t tempo_ligado_ms);
-
+        /**
+         * @brief Desliga o led
+         */
         void desligar();//ok
-
-        void piscar();//ok
+        /**
+         * @brief Pisca o led continuamente
+         */
+        void piscar();
+        /**
+         * @brief Pisca o led em uma determinada frequencia por segundo continuamente
+         * @param frequencia frequencia que o led pisca por segundo
+         */
         void piscar(float frequencia );
+        /**
+         * @brief Pisca o led em uma determinada frequencia por segundo , por uma quantidade determinada de vezes
+         * @param frequencia frequencia que o led pisca por segundo
+         * @param repeticoes determina a quantidade de vezes que o led pisca
+         */
         void piscar(float frequencia, uint16_t repeticoes );
-
-        bool getestado(); //ok
-        uint8_t getpino(); // ok
-
-        void setestado(bool estado); // ok
-
-        void alternar();//ok
-        void update();//ok
+       /**
+         * @brief Salva o estado do led
+         */
+        bool getestado();
+        /**
+         * @brief Salva o pino
+         */
+        uint8_t getpino(); 
+        /**
+         * @brief Ele confirma o estado e o salva em outro atributo
+         * @param estado ele pega o estado do led
+         */
+        void setestado(bool estado);
+        /**
+         * @brief Alterna entre ligar e desligar o led, mais precisamente ele troca o estado do led
+         */
+        void alternar();
+        /**
+         * @brief Ele gerencia as condições para  funções do led
+         */
+        void update();
 };
 #endif
